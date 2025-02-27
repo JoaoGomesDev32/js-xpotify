a26a7e502d018ccac823be75afc31da4cc5a2ced
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-02-27 21:41:47 +0000

    :rocket:
    get api

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -9,6 +9,7 @@ import Sidebar from '../../components/sidebar'
 import '../../components/sidebar/sidebar.css'
 import './home.css'
 import Login from '../auth/login'
+import { setClientToken } from '../../spotify'
 
 export default function Home() {
   const [token, setToken] = useState("");
@@ -21,8 +22,10 @@ export default function Home() {
       const _token = hash.split("&")[0].split("=")[1];
       window.localStorage.setItem("token", _token);
       setToken(_token);
+      setClientToken(_token);
     } else {
       setToken(token);
+      setClientToken(token);
     }
   }, [])
   return !token ? (
